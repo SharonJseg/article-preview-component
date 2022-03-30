02d4f6c3d0d507e5429c6ec2be69b6774def4d06
SharonJseg <sharon.j.segal@gmail.com>
2022-03-31 02:54:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Article preview component
+
+![Article preview component
+](https://drive.google.com/uc?export=view&id=)
+
+[Link to page](https://sharonjseg.github.io/article-preview-component/)
---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <section className='article-preview'>
+        <img src='' alt='' className='article-preview__image' />
+      </section>
+    </div>
+  );
+}
+
+export default App;
